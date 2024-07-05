88c3c549c9e42209ba982de3ac75a163be79e1f9
yuriy <lamukhin.yuriy@gmail.com>
2024-07-05 13:56:45 +0300

    first commit

---
+++ src/test/java/org/testtask/BusinessCard/BusinessCardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testtask.BusinessCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
